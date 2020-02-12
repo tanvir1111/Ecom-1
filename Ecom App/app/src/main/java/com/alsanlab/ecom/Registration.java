8ff34e92b5ea4fdc124e34acc0d9bb233e1f4c15
alsancodeeater <supsanandroid@gmail.com>
2020-02-12 19:11:30 +0600

    registration from two filed

--- Ecom App/app/src/main/java/com/alsanlab/ecom/Registration.java
+++ Ecom App/app/src/main/java/com/alsanlab/ecom/Registration.java
@@ -10,5 +10,6 @@ public class Registration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        getSupportActionBar().hide();
     }
 }
