829ccf724a73aa2c88f423fc97c337d423db99e0
tanvir1024 <tanvir1024@users.noreply.github.com>
2020-02-29 19:54:50 +0600

    Update SplashScreen.java

--- Ecom App/app/src/main/java/com/alsanlab/ecom/SplashScreen.java
+++ Ecom App/app/src/main/java/com/alsanlab/ecom/SplashScreen.java
@@ -9,7 +9,7 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splash_screen);
         getSupportActionBar().hide();
 
     }
