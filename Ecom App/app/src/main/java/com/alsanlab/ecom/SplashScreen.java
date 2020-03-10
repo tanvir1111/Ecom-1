74e2d94f5bbb97628f947758846ae6a11c9b64fb
alsancodeeater <supsanandroid@gmail.com>
2020-03-11 02:17:15 +0600

    Revert "Registration done"
    This reverts commit 3fd5e2c2e47b2f7788f7ae3b8f69afc966e19e57.

--- Ecom App/app/src/main/java/com/alsanlab/ecom/SplashScreen.java
+++ Ecom App/app/src/main/java/com/alsanlab/ecom/SplashScreen.java
@@ -2,11 +2,7 @@ package com.alsanlab.ecom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-
-import com.alsanlab.ecom.reg.Registration;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -15,19 +11,6 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
         getSupportActionBar().hide();
-
-        final Intent it=new Intent(this, Registration.class);
-
-        Handler handler=new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(it);
-                finish();
-
-            }
-        }, 2000);
-
 
     }
 }
--- Ecom App/app/src/main/java/com/alsanlab/ecom/reg/RegVIewModel.java
+++
@@ -1,6 +0,0 @@
-package com.alsanlab.ecom.reg;
-
-import androidx.lifecycle.ViewModel;
-
-public class RegVIewModel extends ViewModel {
-}
